5380f84d224155ea704bb934dbcf3b1001c807b3
Pablo <pperpinyanc@gmail.com>
2022-10-28 19:24:13 +0200

    avances mapa(a pesar del Josep)

---
+++ Assets/atachPlayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class atachPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            player.transform.parent = transform;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            player.transform.parent = null;
+        }
+    }
+}
