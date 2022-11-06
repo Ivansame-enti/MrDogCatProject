cb5e9e066241e0d8c680309ff46457c34d25adee
Ivan Sales <ivan.sales@enti.cat>
2022-11-06 19:56:42 +0100

    gitflow-feature-stash: UI

--- Assets/PickUpController.cs
+++ Assets/PickUpController.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PickUpController : MonoBehaviour
 {
+    private int coinNumber;
+    public TextMeshProUGUI coinText;
+
+    private void Start()
+    {
+        coinNumber = 000;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Coin")
         {
             Destroy(other.gameObject);
+            coinNumber++;
+            coinText.text = coinNumber.ToString("000");
         }
     }
 }
