ec55ae6a499d92815bdfbcf51b446fc5a2c02010
Pablo <pperpinyanc@gmail.com>
2022-11-13 21:04:44 +0100

    nivel jugable 100% no fake

--- Assets/atachPlayer.cs
+++ Assets/atachPlayer.cs
@@ -5,23 +5,54 @@ using UnityEngine;
 public class atachPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject player;
+    public GameObject player1;
+    public GameObject player2;
+
+    private Animation anim;
+
+
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        anim["spin"].layer = 123;
+        anim.Stop("MovePlatform");
+    }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject == player)
+        if(other.gameObject == player1)
         {
-            player.transform.parent = transform;
+            player1.transform.parent = transform;
+        }
+        if (other.gameObject == player2)
+        {
+            player2.transform.parent = transform;
         }
     }
 
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject == player)
+        if (other.gameObject == player1)
         {
-            player.transform.parent = null;
+            player1.transform.parent = null;
+        }
+        if (other.gameObject == player2)
+        {
+            player2.transform.parent = null;
+        }
+    }
+
+
+
+    private void Update()
+    {
+        if(player1.transform.parent==this && player2.transform.parent == this)
+        {
+
+            anim.Play("MovePlatform");
         }
     }
 }
