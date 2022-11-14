43f6fa2b9a42ded4a01df8d20ec55e33e55db2fd
Pablo <pperpinyanc@gmail.com>
2022-11-14 19:42:20 +0100

    Mapa 100% acabado no fake 1 link mega

--- Assets/atachPlayer.cs
+++ Assets/atachPlayer.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class atachPlayer : MonoBehaviour
@@ -9,19 +7,33 @@ public class atachPlayer : MonoBehaviour
     public GameObject player2;
     public GameObject cube1;
     public GameObject cube2;
- 
+    public GameObject originalPadre;
+    private bool startea = false;
+    private bool gatofuera = false;
+    private bool perrofuera = false;
+    public float timer;
+    public float cooldown = 5.0f;
 
 
-
+    private void Start()
+    {
+        timer = cooldown;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject == player1)
         {
             player1.transform.parent = transform;
+            gatofuera = false;
+            timer = cooldown;
+            //player1.transform.localScale = new Vector3(1, 1, 1);
         }
         if (other.gameObject == player2)
         {
             player2.transform.parent = transform;
+            perrofuera = false;
+            timer = cooldown;
+            // player2.transform.localScale = new Vector3(1, 1, 1);
         }
     }
 
@@ -31,10 +43,16 @@ public class atachPlayer : MonoBehaviour
         if (other.gameObject == player1)
         {
             player1.transform.parent = null;
+            player1.transform.parent = originalPadre.transform;
+            player1.transform.localScale = new Vector3(1, 1, 1);
+            gatofuera = true;
         }
         if (other.gameObject == player2)
         {
             player2.transform.parent = null;
+            player2.transform.parent = originalPadre.transform;
+            player2.transform.localScale = new Vector3(1,1,1);
+            perrofuera = true;
         }
     }
 
@@ -42,15 +60,57 @@ public class atachPlayer : MonoBehaviour
 
     private void Update()
     {
-        if(player1.transform.parent == cube1 && player2.transform.parent == cube2)//CADA UNO EN UNO DISTINTO perro izquierda gato derecha
+        if(player1.transform.parent == cube1.transform && player2.transform.parent == cube2.transform)//CADA UNO EN UNO DISTINTO perro izquierda gato derecha
         {
-            Debug.Log("entra");
+            startea = true;
             this.GetComponent<Animator>().SetBool("Move", true);
+
           
+           
+            if (player1.transform.parent == cube1.transform || player1.transform.parent == cube2.transform)
+            {
+                this.GetComponent<Animator>().SetBool("Move", true);
+            }
+
+
+            if (player2.transform.parent == cube1.transform || player2.transform.parent == cube2.transform)
+            {
+                this.GetComponent<Animator>().SetBool("Move", true);
+            }
+
+           
+
+
+        }
+        if (startea == true) { 
+            if (player1.transform.parent == originalPadre.transform && player2.transform.parent == originalPadre.transform)
+            {
+
+                startea = false;
+            }
+        }
+        else if(startea == false)
+        {
+           
+
+            if (timer <= 0.0f) { 
+                this.GetComponent<Animator>().SetBool("Move", false);
+                timer = cooldown;
+            }else timer -= Time.deltaTime;
+            // player1.transform.parent = originalPadre.transform;
+            // player2.transform.parent = originalPadre.transform;
         }
         else
         {
-            this.GetComponent<Animator>().SetBool("Move", false);
+            timer = cooldown;
+        }
+
+
+        if(gatofuera==true && perrofuera==true)
+        {
+           
+             //player1.transform.parent = originalPadre.transform;
+           // player2.transform.parent = originalPadre.transform;
         }
     }
 }
