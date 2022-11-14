100831b16a665536c448c5024149a9ba4a2189d2
Ivan Sales <ivan.sales@enti.cat>
2022-11-14 11:05:29 +0100

    Mas avances en el enemigo

--- Assets/Enemy2Controller.cs
+++ Assets/Enemy2Controller.cs
@@ -8,11 +8,14 @@ public class Enemy2Controller : MonoBehaviour
     private Transform player1, player2;
     private bool move=true;
     public float speed;
+    private float timer;
+    public float timeBettwenAttacks;
     //private NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
+        timer = timeBettwenAttacks;
         player1 = GameObject.Find("Dog").transform;
         player2 = GameObject.Find("Cat").transform;
         //agent = this.GetComponent<NavMeshAgent>();
@@ -21,12 +24,12 @@ public class Enemy2Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (move)
+        if (timer>=timeBettwenAttacks)
         {
             this.GetComponent<Animator>().SetInteger("Walk", 1);
             Vector3 distanceToPlayerOne = this.transform.position - player1.position;
             Vector3 distanceToPlayerTwo = this.transform.position - player2.position;
-
+            move = true;
             if (distanceToPlayerOne.magnitude <= distanceToPlayerTwo.magnitude)
             {
                 var lookDir = player1.position - transform.position;
@@ -41,7 +44,9 @@ public class Enemy2Controller : MonoBehaviour
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookDir), 100 * Time.deltaTime);
                 transform.position = Vector3.MoveTowards(transform.position, player2.position, speed * Time.deltaTime);
             }
+            timer = 0;
         }
+        else timer += Time.deltaTime;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -55,6 +60,7 @@ public class Enemy2Controller : MonoBehaviour
             collision.gameObject.GetComponent<Rigidbody>().AddForce(direction * 50, ForceMode.Impulse);
             //this.GetComponent<Rigidbody>().AddForce(-direction * 100, ForceMode.Impulse);
             move = false;
+            timer = 0;
         }
     }
 
@@ -62,7 +68,7 @@ public class Enemy2Controller : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            this.GetComponent<Rigidbody>().mass = 0.1f;
+            if(move) this.GetComponent<Rigidbody>().mass = 0.1f;
             //move = true;
         }
     }
