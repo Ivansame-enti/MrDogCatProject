92325c7440fbe397aaf79d535b39617daa8d1fd8
Ivan Sales <ivan.sales@enti.cat>
2022-11-14 10:28:22 +0100

    Avances enemigo 2

--- Assets/Enemy2Controller.cs
+++ Assets/Enemy2Controller.cs
@@ -7,6 +7,7 @@ public class Enemy2Controller : MonoBehaviour
 {
     private Transform player1, player2;
     private bool move=true;
+    public float speed;
     //private NavMeshAgent agent;
 
     // Start is called before the first frame update
@@ -23,31 +24,22 @@ public class Enemy2Controller : MonoBehaviour
         if (move)
         {
             this.GetComponent<Animator>().SetInteger("Walk", 1);
-            //agent.speed = 3.0f;
             Vector3 distanceToPlayerOne = this.transform.position - player1.position;
             Vector3 distanceToPlayerTwo = this.transform.position - player2.position;
 
             if (distanceToPlayerOne.magnitude <= distanceToPlayerTwo.magnitude)
             {
-                //transform.LookAt(player1);
-                //agent.SetDestination(player1.position);
-                //Quaternion targetRotation = Quaternion.LookRotation(player1.position - transform.position);
-                //transform.rotation = Quaternion.RotateTowards(transform.rotation, new Quaternion(0, targetRotation.y, targetRotation.z, targetRotation.w), 100 * Time.deltaTime);
-
                 var lookDir = player1.position - transform.position;
-                lookDir.y = 0; // keep only the horizontal direction
-                               //transform.rotation = Quaternion.LookRotation(lookDir);
+                lookDir.y = 0;
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookDir), 100 * Time.deltaTime);
                 transform.position = Vector3.MoveTowards(transform.position, player1.position, 20 * Time.deltaTime);
             }
             else
             {
-                //transform.LookAt(player2);
-                //agent.SetDestination(player2.position);
                 var lookDir = player2.position - transform.position;
-                lookDir.y = 0; // keep only the horizontal direction
+                lookDir.y = 0;
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookDir), 100 * Time.deltaTime);
-                transform.position = Vector3.MoveTowards(transform.position, player2.position, 20 * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, player2.position, speed * Time.deltaTime);
             }
         }
     }
@@ -56,7 +48,22 @@ public class Enemy2Controller : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
+            this.GetComponent<Rigidbody>().mass = 50;
+            Vector3 direction = (collision.transform.position - transform.position).normalized;
+
+            direction.y = 0;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(direction * 50, ForceMode.Impulse);
+            //this.GetComponent<Rigidbody>().AddForce(-direction * 100, ForceMode.Impulse);
             move = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            this.GetComponent<Rigidbody>().mass = 0.1f;
+            //move = true;
         }
     }
 }
