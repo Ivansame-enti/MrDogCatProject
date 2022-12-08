4b0baaa4442db4072589c64ea4f6ef8c6f1e9df9
Josep <josep.romera@enti.cat>
2022-12-08 22:45:09 +0100

    Nivel 1 avanzado

---
+++ Assets/Project/Scripts/Envoirament/LightFloor.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFloor : MonoBehaviour
+{
+    private bool returnFloor;
+    private float timer;
+    public GameObject floor;
+    private GameObject clone;
+    private LightFloorCollision lightFloorCollision;
+    private MeshRenderer rend;
+    public float plataformDownTime, respawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        rend = GetComponent<MeshRenderer>();
+
+    }
+    private void Awake()
+    {
+        //rend.enabled = false;
+        clone = Instantiate(floor, gameObject.transform.position, Quaternion.identity);
+        lightFloorCollision = clone.GetComponent<LightFloorCollision>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lightFloorCollision.detectPlayer)
+        {
+            Debug.Log("buenas");
+            Destroy(clone, plataformDownTime);
+            lightFloorCollision.detectPlayer = false;
+            returnFloor = true;
+        }
+        if(returnFloor)
+        {
+            Debug.Log("ola");
+            timer += Time.deltaTime;
+            if(timer >= respawnTime)
+            {
+                returnFloor = false;
+                clone = Instantiate(floor,transform.position,Quaternion.identity);
+                lightFloorCollision = clone.GetComponent<LightFloorCollision>();
+                timer = 0;
+            }
+        }
+
+    }
+
+
+ }
---
+++ Assets/Project/Scripts/Envoirament/LightFloorCollision.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFloorCollision : MonoBehaviour
+{
+    public bool detectPlayer;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            detectPlayer = true;
+        }
+    }
+}
