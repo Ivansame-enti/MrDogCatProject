eca3aec1666b10cdf81dd4cdffe32071e0b50d21
Josep <josep.romera@enti.cat>
2022-12-15 19:06:23 +0100

    Develop sin gallina ni pinguino

--- Assets/Project/Scripts/Players/PickUpController.cs
+++ Assets/Project/Scripts/Players/PickUpController.cs
@@ -6,17 +6,13 @@ using TMPro;
 public class PickUpController : MonoBehaviour
 {
     private int objective1Number, objective2Number;
-    public TextMeshProUGUI coinText, objective1Text, objective2Text;
+    public TextMeshProUGUI coinText;
     public int coinNumber;
     private AudioManagerController audioSFX;
-    public AudioSource chickenSound, penguinSound;
     private void Start()
     {
         audioSFX = FindObjectOfType<AudioManagerController>();
         coinNumber = 000;
-        objective1Number = 0;
-        objective2Number = 0;
-
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,46 +23,6 @@ public class PickUpController : MonoBehaviour
             coinNumber++;
             coinText.text = coinNumber.ToString("000");
             audioSFX.AudioPlay("Coin");
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Flee")
-        {
-            if (!collision.gameObject.GetComponent<FleeController>().isPicked)
-            {
-                collision.gameObject.GetComponent<FleeController>().isPicked = true;
-                collision.gameObject.GetComponent<FleeController>().agent.isStopped = true;
-                collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-                collision.gameObject.GetComponent<Animator>().SetBool("isDead", true);
-                objective2Number++;
-                objective2Text.text = objective2Number.ToString();
-                coinNumber+=7;
-                coinText.text = coinNumber.ToString("000");
-                penguinSound.Stop();
-                audioSFX.AudioPlay("PenguinDeath");
-                Destroy(collision.gameObject, 2f);
-            }
-        }
-
-        if (collision.gameObject.tag == "Flee2")
-        {
-            if (!collision.gameObject.GetComponent<FleeController>().isPicked)
-            {
-                collision.gameObject.GetComponent<FleeController>().isPicked = true;
-                collision.gameObject.GetComponent<FleeController>().agent.isStopped = true;
-                collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-                collision.gameObject.GetComponent<Animator>().SetBool("isDead", true);
-                objective1Number++;
-                objective1Text.text = objective1Number.ToString();
-                coinNumber += 7;
-                coinText.text = coinNumber.ToString("000");
-                chickenSound.Stop();
-                audioSFX.AudioPlay("ChickenDeath");
-                Debug.Log("ola");
-                Destroy(collision.gameObject, 2f);
-            }
         }
     }
 }
