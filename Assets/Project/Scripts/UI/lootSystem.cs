7fa23b6a938577ed99862efa8a327105af5f15bb
Pablo <pperpinyanc@gmail.com>
2022-12-19 13:19:28 +0100

    A pesar del josep, las lootboxes siguen mas vivas que nunca :)

--- Assets/Project/Scripts/UI/lootSystem.cs
+++ Assets/Project/Scripts/UI/lootSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [System.Serializable]
 public class ItemToSpawn
@@ -9,6 +10,7 @@ public class ItemToSpawn
     public GameObject[] item;
     public float spawRate;
     [HideInInspector] public float minSpawnProb, maxSpawnProb;
+  
 
 }
 
@@ -16,10 +18,15 @@ public class lootSystem : MonoBehaviour
 {
     // Start is called before the first frame update
     public ItemToSpawn[] itemToSpawn;
- 
+    public GameObject comunParticles, LittlecomunParticles, RareParticles, EpicParticles, LegendaryParticles;
+    GameObject hat;
+    //private float monedas;
+    public TextMeshProUGUI CoinText;
     void Start()
     {
 
+        StaticClass.CoinsDog = 100000;
+        CoinText.text = StaticClass.CoinsDog.ToString("000");
         for (int i=0; i<itemToSpawn.Length;i++)
         {
             if (i == 0)
@@ -36,17 +43,37 @@ public class lootSystem : MonoBehaviour
 
         }
 
-       Spawner();
+        if( StaticClass.CoinsDog >= 10) {
+            StaticClass.CoinsDog -= 10;
+            CoinText.text = StaticClass.CoinsDog.ToString("000");
+            Spawner();
+        }
+
+    }
+    public void Reroll()
+    {
+        Debug.Log("HELICOPTERO");
+
+        if (StaticClass.CoinsDog >= 10)
+        {
+            comunParticles.SetActive(false);
+            LittlecomunParticles.SetActive(false);
+            EpicParticles.SetActive(false);
+            RareParticles.SetActive(false);
+            LegendaryParticles.SetActive(false);
+
+            StaticClass.CoinsDog -= 10;
+            CoinText.text = StaticClass.CoinsDog.ToString("000");
+            Spawner();
+        }
+
 
     }
 
     void Update()
     {
+      
 
-     //   if (Input.GetKeyDown(KeyCode.E))
-      //  {
-       //     Spawner();
-     //   }
 
 
 
@@ -54,6 +81,14 @@ public class lootSystem : MonoBehaviour
 
     void Spawner()
     {
+
+        if (hat!=null){
+
+            Destroy(hat);
+        }
+     
+
+
         float randomNum = Random.Range(0, 100);
         int randomNum2 = 0;
         for (int i=0; i<itemToSpawn.Length; i++)
@@ -66,36 +101,61 @@ public class lootSystem : MonoBehaviour
                 if (i == 0)
                 {
                     Debug.Log("Comun");
-                   randomNum2 = Random.Range(0, itemToSpawn[i].item.Length);
+                    comunParticles.SetActive(true);
+                    LittlecomunParticles.SetActive(false);
+                    EpicParticles.SetActive(false);
+                    RareParticles.SetActive(false);
+                    LegendaryParticles.SetActive(false);
+                    randomNum2 = Random.Range(0, itemToSpawn[i].item.Length);
                     Debug.Log(randomNum2);
-                    GameObject hat = Instantiate(itemToSpawn[i].item[randomNum2], transform.position, itemToSpawn[i].item[randomNum2].transform.rotation);
+                     hat = Instantiate(itemToSpawn[i].item[randomNum2], transform.position, itemToSpawn[i].item[randomNum2].transform.rotation);
                     hat.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
                 }
                 else if (i==1)
                 {
                     Debug.Log("Poco Comun");
+                    comunParticles.SetActive(false);
+                    LittlecomunParticles.SetActive(true);
+                    EpicParticles.SetActive(false);
+                    RareParticles.SetActive(false);
+                    LegendaryParticles.SetActive(false);
                     randomNum2 = Random.Range(0, itemToSpawn[i].item.Length);
-                    GameObject hat = Instantiate(itemToSpawn[i].item[randomNum2], transform.position, itemToSpawn[i].item[randomNum2].transform.rotation);
+                     hat = Instantiate(itemToSpawn[i].item[randomNum2], transform.position, itemToSpawn[i].item[randomNum2].transform.rotation);
                     hat.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
                 }
                 else if (i == 2)
                 {
                     Debug.Log("Rara");
-                    GameObject hat = Instantiate(itemToSpawn[i].item[0], transform.position, itemToSpawn[i].item[randomNum2].transform.rotation);
+                    comunParticles.SetActive(false);
+                    LittlecomunParticles.SetActive(false);
+                    EpicParticles.SetActive(false);
+                    RareParticles.SetActive(true);
+                    LegendaryParticles.SetActive(false);
+                     hat = Instantiate(itemToSpawn[i].item[0], transform.position, itemToSpawn[i].item[randomNum2].transform.rotation);
                     hat.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
                 }
                 else if (i == 3)
                 {
                     Debug.Log("EPICA DORADA");
+                    comunParticles.SetActive(false);
+                    LittlecomunParticles.SetActive(false);
+                    EpicParticles.SetActive(true);
+                    RareParticles.SetActive(false);
+                    LegendaryParticles.SetActive(false);
                     randomNum2 = Random.Range(0, itemToSpawn[i].item.Length);
-                    GameObject hat = Instantiate(itemToSpawn[i].item[randomNum2], transform.position, itemToSpawn[i].item[randomNum2].transform.rotation);
+                     hat = Instantiate(itemToSpawn[i].item[randomNum2], transform.position, itemToSpawn[i].item[randomNum2].transform.rotation);
                     hat.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
                 }
                 else if (i == 4)
                 {
+                    comunParticles.SetActive(false);
+                    LittlecomunParticles.SetActive(false);
+                    EpicParticles.SetActive(false);
+                    RareParticles.SetActive(false);
+                    LegendaryParticles.SetActive(true);
                     Debug.Log("WOOOOOOW LEGENDARIAAAAAAAAAAAA");
                     randomNum2 = Random.Range(0, itemToSpawn[i].item.Length);
-                    GameObject hat = Instantiate(itemToSpawn[i].item[randomNum2], transform.position, itemToSpawn[i].item[randomNum2].transform.rotation);
+                     hat = Instantiate(itemToSpawn[i].item[randomNum2], transform.position, itemToSpawn[i].item[randomNum2].transform.rotation);
                     hat.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
                 }
 
