043e2bb90bb7d0856b38a9434174dced06d9f1e7
Ivan Sales <ivan.sales@enti.cat>
2023-01-01 19:51:35 +0100

    Añadido el hueso al nivel

--- Assets/Project/Scripts/Players/PickUpController.cs
+++ Assets/Project/Scripts/Players/PickUpController.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class PickUpController : MonoBehaviour
 {
     private TextMeshProUGUI coinText;
+    private Image collectImage;
     public int coinNumber;
     private AudioManagerController audioSFX;
     private void Start()
     {
         if(this.tag=="Dog") coinText = GameObject.FindGameObjectWithTag("DogCoinText").GetComponent<TextMeshProUGUI>();
         if (this.tag == "Cat") coinText = GameObject.FindGameObjectWithTag("CatCoinText").GetComponent<TextMeshProUGUI>();
+        collectImage = GameObject.FindGameObjectWithTag("CollectImage").GetComponent<Image>();
         audioSFX = FindObjectOfType<AudioManagerController>();
         coinNumber = 000;
     }
@@ -24,6 +27,12 @@ public class PickUpController : MonoBehaviour
             coinNumber++;
             coinText.text = coinNumber.ToString("000");
             audioSFX.AudioPlay("Coin");
+        }
+
+        if (other.gameObject.tag == "Bone" && this.tag=="Dog")
+        {
+            Destroy(other.gameObject);
+            collectImage.color = new Color(255, 255, 255, 255);
         }
     }
 }
