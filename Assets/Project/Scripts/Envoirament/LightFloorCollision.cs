7c4d869a966a414552fb53c509c95428d25a4623
Josep <josep.romera@enti.cat>
2023-01-07 19:59:37 +0100

    Cosas

--- Assets/Project/Scripts/Envoirament/LightFloorCollision.cs
+++ Assets/Project/Scripts/Envoirament/LightFloorCollision.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class LightFloorCollision : MonoBehaviour
 {
     public bool detectPlayer;
+    private Animator animator;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Cat" || collision.gameObject.tag == "Dog")
         {
             detectPlayer = true;
+            animator.enabled = true;
         }
     }
 }
