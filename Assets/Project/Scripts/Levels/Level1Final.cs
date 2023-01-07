5397cf948317f40d53ae6c470a6015f3c0c075be
Josep <josep.romera@enti.cat>
2023-01-07 21:09:45 +0100

    Commit

--- Assets/Project/Scripts/Levels/Level1Final.cs
+++ Assets/Project/Scripts/Levels/Level1Final.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using static System.TimeZoneInfo;
+using UnityEditor.SearchService;
 
 public class Level1Final : MonoBehaviour
 {
     public GameObject flag;
     private TextMeshProUGUI coinTextDog;
     private TextMeshProUGUI coinTextCat;
+
+    [SerializeField]
+    private Material transitionMaterial;
+
+    [SerializeField]
+    private float transitionTime = 3f;
+
+    [SerializeField]
+    private string propertyName = "_Progress";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +31,23 @@ public class Level1Final : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(flag.transform.position.y >= 10)
+        if (flag.transform.position.y >= 10)
         {
-            StaticClass.CoinsCat = int.Parse(coinTextCat.text);
-            StaticClass.CoinsDog = int.Parse(coinTextDog.text);
-            SceneManager.LoadScene("Win");
+            StartCoroutine(FinishTranition());
         }
+    }
+
+    private IEnumerator FinishTranition()
+    {
+        float currentTime = transitionTime;
+        while (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            transitionMaterial.SetFloat(propertyName, Mathf.Clamp01(currentTime / transitionTime));
+            yield return null;
+        }
+        StaticClass.CoinsCat = int.Parse(coinTextCat.text);
+        StaticClass.CoinsDog = int.Parse(coinTextDog.text);
+        SceneManager.LoadScene("Win");
     }
 }
